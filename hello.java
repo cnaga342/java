64e87d62b11564ea189cced70f4e9dd301a43089
Nagabhushanam <cnaga342@gmail.com>
2024-03-26 08:26:19 +0530

    hello java

---
+++ hello.java
@@ -0,0 +1,25 @@
+// Print Hello World
+
+// Description
+// You need to print "Hello World".
+
+// Input
+// No input
+
+// Output
+// Print "Hello World" on a line
+
+// .
+
+// Sample Input 1 []
+
+// No Input
+
+// Sample Output 1
+
+// Hello World
+
+public static void main(String[] args) {
+    System.out.println(Hello word);
+   
+}
