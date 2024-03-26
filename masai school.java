d398a86a9e0b2726030948641aa9b6fa7809856a
Nagabhushanam <cnaga342@gmail.com>
2024-03-26 08:49:10 +0530

    masai school

---
+++ masai school.java
@@ -0,0 +1,29 @@
+// Print Masai School
+
+// Description
+// You need to print "Masai School" on a line, and print "A Transformation
+// in Education" on a new line.
+
+// Input
+// No input
+
+// Output
+// Print "Masai School" on a line, and Print "A Transformation in Educatio
+// n" on a new line.
+
+// . Ended
+
+// Sample Input 1 ]
+
+// No Input
+
+// Sample Output 1
+
+// Masai School
+// A Transformation in Education
+
+public static void PrintMasaiSchool{
+   //write your code here
+   System.out.println("Masai School");
+   System.out.println("A Transformation in Education");
+  }
